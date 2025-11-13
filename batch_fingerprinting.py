702466c316fa74160e6d7ef9cf9dd295426e8f71
Muhammad Waqas <57534149+waqastakkar@users.noreply.github.com>
2025-11-13 17:11:38 +0800

    Update batch_fingerprinting.py

--- batch_fingerprinting.py
+++ batch_fingerprinting.py
@@ -9,7 +9,7 @@ Permission is granted to use this code for academic and research purposes only.
 Modification, redistribution, or commercial use is not permitted.
 
 Any published work or research that uses this code must cite:
-Waqas, M. (2025). QSAR Modeling and Virtual Screening Pipeline.
+Waqas, M. (2025). Structural and Dynamic Profiling of SARS-CoV-2 Papain-Like Protease Bound to Machine-Learning-Identified Oxadiazole Inhibitors.
 
 batch_fingerprinting.py
 Interactive batch creation of RDKit fingerprints from many SMILES tables (CSV/TSV/XLSX).
